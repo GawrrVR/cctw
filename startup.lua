e9008b2c474f17403185645756d96a3b18e00ee4
Gawrr <64701051+GawrrVR@users.noreply.github.com>
2025-11-27 10:36:13 +0100

    Update colors and bar length in startup.lua

--- startup.lua
+++ startup.lua
@@ -164,33 +164,34 @@ function print_matrix_info (matrix_info)
   print_r(border, colors.white)
   print_r(bordered_line("", colors.white))
 
-  local bar_length = 10
+  local bar_length = 15
   local filled = math.floor(matrix_info.energy_percentage * bar_length)
   local bar = "[" .. string.rep("=", filled) .. string.rep(" ", bar_length - filled) .. "]"
-  print_r(bordered_line("Puissance: " .. energy_string(matrix_info.energy_stored), colors.white))
-  print_r(bordered_line("Limite: " .. energy_string(matrix_info.energy_capacity), colors.white))
-  print_r(bordered_line("Charge: " .. round_percentage(matrix_info.energy_percentage), colors.white))
-  print_r(bordered_line(bar, colors.white))
+  print_r(bordered_line("Puissance: " .. energy_string(matrix_info.energy_stored), colors.green))
+  print_r(bordered_line("Limite: " .. energy_string(matrix_info.energy_capacity), colors.green))
+  print_r(bordered_line("Charge: " .. round_percentage(matrix_info.energy_percentage), colors.green))
+  print_r(bordered_line(bar, colors.yellow))
   print_r(bordered_line("", colors.white))
-  print_r(bordered_line("Input: " .. energy_string(matrix_info.io_input) .. "/t", colors.white))
-  print_r(bordered_line("Output: " .. energy_string(matrix_info.io_output) .. "/t", colors.white))
-  print_r(bordered_line("Max IO: " .. energy_string(matrix_info.io_capacity) .. "/t", colors.white))
+  print_r(bordered_line("Input: " .. energy_string(matrix_info.io_input) .. "/t", colors.blue))
+  print_r(bordered_line("Output: " .. energy_string(matrix_info.io_output) .. "/t", colors.blue))
+  print_r(bordered_line("Max IO: " .. energy_string(matrix_info.io_capacity) .. "/t", colors.blue))
   print_r(bordered_line("", colors.white))
 
   local change_text = "Change: " .. energy_string(matrix_info.change_amount_per_second) .. "/s"
-  print_r(bordered_line(change_text, colors.white))
+  print_r(bordered_line(change_text, colors.purple))
   print_r(bordered_line("", colors.white))
 
   local status_text = "Status: "
   local status_color = colors.white
   if matrix_info.is_charging then
     status_text = status_text .. "Charge. " .. eta_string((matrix_info.energy_capacity - matrix_info.energy_stored) / matrix_info.change_amount_per_second)
-    status_color = colors.green
+    status_color = colors.lime
   elseif matrix_info.is_discharging then
     status_text = status_text .. "Décharge. " .. eta_string(matrix_info.energy_stored / math.abs(matrix_info.change_amount_per_second))
     status_color = colors.red
   else
     status_text = status_text .. "~"
+    status_color = colors.gray
   end
   print_r(bordered_line(status_text, status_color))
   print_r(border, colors.white)
